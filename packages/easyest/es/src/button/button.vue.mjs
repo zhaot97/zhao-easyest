47181da565db346dd82654a3397a8287eb45bca2
zhaot97 <17088097497@163.com>
2023-08-11 17:08:28 +0800

    使用Vite打包组件库， 通过vite-plugin-dts插件让打包后的文件自动生成声明文件， 并让样式文件也支持按需引入

---
+++ packages/easyest/es/src/button/button.vue.mjs
@@ -0,0 +1,19 @@
+import { defineComponent as e, computed as a, openBlock as s, createElementBlock as u, normalizeClass as l, renderSlot as p } from "vue";
+import "./style/index.less.mjs";
+const c = e({ name: "ea-button" }), d = /* @__PURE__ */ e({
+  ...c,
+  props: {
+    type: {}
+  },
+  setup(o) {
+    const t = o, n = a(() => ({ [`ea-button--${t.type}`]: t.type }));
+    return (r, m) => (s(), u("button", {
+      class: l(["ea-button", n.value])
+    }, [
+      p(r.$slots, "default")
+    ], 2));
+  }
+});
+export {
+  d as default
+};
